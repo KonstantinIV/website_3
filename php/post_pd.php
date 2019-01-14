44a499803e10a5a1212c13b595aa14707932eff0
Konstantin <konstantin-r@hotmail.com>
2019-01-14 14:58:11 +0200

    Date

--- php/post_pd.php
+++ php/post_pd.php
@@ -1,11 +1,13 @@
 <?php 
 
-function san_dt($title,$text){
+function san_dt($title,$date,$text){
     //Check if text contains html tags , ex <div>
     if($text != strip_tags($text) ||  $title != strip_tags($title)){
         echo "Contains html tags";
-    }else{
-        ins_dt($title,$text);
+    }else if(!is_int($date) || strlen((string)$date) != 8){
+        echo "Date is not numeric or wrong input";
+    }else {
+        ins_dt($title,$date,$text);
 
     }
 }
@@ -31,16 +33,17 @@ function con_db(){
 }
 
 //insert function
-function ins_dt($title, $text){
+function ins_dt($title,$date, $text){
     //Pdo object
     $pdo = con_db();
     //Check if connection was succesful before inserting
     if ($pdo){
-        $stmt = $pdo->prepare('insert into post (name, text, c_date ) values (?,?, DATE_FORMAT(now(), "%Y-%m-%d %H:%i") )');
-        $stmt->execute([$title,$text]);
-        //$data = $stmt->fetch(PDO::FETCH_ASSOC);
+        //echo strlen((string)$date);
+        $stmt = $pdo->prepare('insert into post (name, text, c_date, pro_date ) values (?,?, DATE_FORMAT(now(), "%Y-%m-%d %H:%i"), ? )');
+        $stmt->execute([$title,$text,$date]);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-
+        echo $data;
         echo "Success";
     }else {
         // ERROR
@@ -56,7 +59,7 @@ function ins_dt($title, $text){
 //Check if user sends get or post important
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //Start
-    san_dt( $_POST['title'], $_POST['text']);
+    san_dt( $_POST['title'],(int)$_POST['date'], $_POST['text']);
 }
 
 
