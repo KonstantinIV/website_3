0e2d4565c0b698b1cab826082d489dc22ea1c24c
Konstantin <konstantin-r@hotmail.com>
2019-02-27 16:56:55 +0200

    rec ful done working version

--- php/test.php
+++ php/test.php
@@ -12,23 +12,25 @@ foreach($i = 0; $i < 3; $i++){
 
 
 function rec($id_pos,$space){
-    $id        = array(1   , 2, 3,4,5,6,7,8,9,10);
-
+    $id        = array(1   , 2, 3,4,5,65,7,8,9,10);
     $parent_id = array(NULL, 1, 2,3,3,3,3,1,1,1);
-    
-    for($i = 9; $i >= 0; $i--){
-        if($parent_id[$i] == $id[$id_pos]){
-            rec($i,$space+1);
-        }
-      
-        
-    }
+
+    /* echo result */
     for($i = 0; $i <= $space; $i++){
         echo "*";
     }
-    //echo "\n".$space."  ****  ".$id[$id_pos]."\r\n";
     echo $id[$id_pos];
     echo "<br>";
+    /***************/
+
+
+    /*core*/
+    for($i = 0; $i < 10; $i++){
+        if($parent_id[$i] == $id[$id_pos]){
+            rec($i,$space+1);
+        }
+    }
+   
 
 }
 rec(0,0);
